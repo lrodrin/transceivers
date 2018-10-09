e09bd2dc26d5c443ab8fc53f722b23abc47baf5a
lrodrin <lrodrin@gmail.com>
2018-10-09 11:06:03 +0200

    new test directory

--- restconf/send_json.py
+++ restconf/send_json.py
@@ -3,33 +3,32 @@
 import argparse
 import sys
 
-from ncclient import manager
+import requests
 
-from netconf.defaults import *
-from netconf.helpers import *
+from restconf.defaults import *
+from restconf.helpers import *
 
 __author__ = "Laura Rodriguez Navas <laura.rodriguez@cttc.cat>"
 __copyright__ = "Copyright 2018, CTTC"
 
 
-def edit_config(xml, host, port, user, pwd):
-    with manager.connect(host=host, port=port, username=user, password=pwd,
-                         hostkey_verify=False, device_params={'name': 'default'}) as m:
-        reply = m.edit_config(target='running', config=xml)
-    return reply
+def post(json, host, port, user, pwd):
+    url = 'http://' + host + ':' + str(port) + '/v1.0/topology/switches'
+    response = requests.post(url, json, auth=(user, pwd))
+    print(response.json)
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--topology', '-t', default=XML, help="XML Topology configuration file")
+    parser.add_argument('--topology', '-t', default=JSON, help="JSON Topology configuration file")
     parser.add_argument('--host', '-H', default=HOST, help="remote host address")
     parser.add_argument('--port', '-P', default=PORT, help="port")
     parser.add_argument('--user', '-u', default=USER, help="username")
     parser.add_argument('--password', '-p', default=PASS, help="password")
     args = parser.parse_args()
 
-    xml = read_file(args.interface)
-    reply = edit_config(xml, args.host, args.port, args.user, args.password)
+    json = read_file(args.topology)
+    reply = post(json, args.host, args.port, args.user, args.password)
     print('Command sent: {}'.format(reply.ok))
 
 
