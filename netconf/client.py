3abb667812724cf6d591cd51390da8f1ea4751a1
lrodrin <lrodrin@gmail.com>
2018-10-16 09:58:08 +0200

    update netconf server.py and client.py

--- netconf/client.py
+++ netconf/client.py
@@ -1,4 +1,19 @@
+from xml.etree import ElementTree
+
 from netconf.client import NetconfSSHSession
 
-session = NetconfSSHSession('127.0.0.1', 830, 'admin', 'admin')
-config = session.get()
+host = '10.1.7.64'
+port = 830
+username = "admin"
+password = "admin"
+
+session = NetconfSSHSession(host, port, username, password)
+capa = session.capabilities
+print(capa)
+
+sid = session.session_id
+print(sid)
+
+config = session.get_config()
+xmlstr = ElementTree.tostring(config, encoding='utf8', method='xml')
+print(xmlstr)
--- netconf/server.py
+++ netconf/server.py
@@ -1,30 +1,51 @@
-from netconf import nsmap_update, server
-import netconf.util as ncutil
+import argparse
 import sys
 import time
 
-MODEL_NS = "urn:ietf:params:xml:ns:yang:ietf-system"
-nsmap_update({'sys': MODEL_NS})
+from netconf import nsmap_add, NSMAP
+from netconf import server, util
 
-class MyServer (object):
-    def __init__ (self, user, pw):
-        controller = server.SSHUserPassController(username=user, password=pw)
-        self.server = server.NetconfSSHServer(server_ctl=controller, server_methods=self)
+nsmap_add("node-topology", "urn:node-topology")
 
-    def nc_append_capabilities(self, caps):
-        ncutil.subelm(caps, "capability").text = MODEL_NS
 
-    def rpc_my_cool_rpc (self, session, rpc, *params):
-        data = ncutil.elm("data")
-        data.append(ncutil.leaf_elm("pfx:result", "RPC result string"))
-        return data
+class MyServer(object):
+    def __init__(self, username, password, port):
+        auth = server.SSHUserPassController(username=username, password=password)
+        self.server = server.NetconfSSHServer(server_ctl=auth, server_methods=self, port=port, debug=False)
+
+    def close(self):
+        self.server.close()
+
+    def nc_append_capabilities(self, capabilities):  # pylint: disable=W0613
+        util.subelm(capabilities, "capability").text = "urn:ietf:params:netconf:capability:xpath:1.0"
+        util.subelm(capabilities, "capability").text = NSMAP["node-topology"]
+
+    def rpc_get_config(self, session, rpc, source_elm, filter_or_none):  # pylint: disable=W0613
+        """Passed the source element"""
+        data = util.elm("nc:data")
+        sysc = util.subelm(data, "node-topology:node")
+        sysc.append(util.leaf_elm("node-topology:node-id", '10.1.7.64'))
+
+        # Clock
+        # clockc = util.subelm(sysc, "sys:clock")
+        # tzname = time.tzname[time.localtime().tm_isdst]
+        # clockc.append(util.leaf_elm("sys:timezone-utc-offset", int(time.timezone / 100)))
+
+        return util.filter_results(rpc, data, filter_or_none)
+
 
 def main(*margs):
+    parser = argparse.ArgumentParser("Example Netconf Server")
+    parser.add_argument("--username", default="admin", help='Netconf username')
+    parser.add_argument("--password", default="admin", help='Netconf password')
+    parser.add_argument('--port', type=int, default=830, help='Netconf server port')
+    args = parser.parse_args(*margs)
 
-    server = MyServer("admin", "admin")
+    s = MyServer(args.username, args.password, args.port)
 
     if sys.stdout.isatty():
         print("^C to quit server")
+    # noinspection PyBroadException
     try:
         while True:
             time.sleep(1)
