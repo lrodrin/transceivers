ba31d0cc08e49e15b087882a163c1cbf861266db
lrodrin <lrodrin@gmail.com>
2018-10-29 17:41:14 +0100

    first solution implemented

--- Netconf/server.py
+++ Netconf/server.py
@@ -7,6 +7,7 @@ from netconf import server, util
 from pyangbind.lib.serialise import pybindIETFXMLEncoder
 from xml.etree import ElementTree
 from binding import node_topology
+from helpers import *
 
 import logging
 logging.basicConfig(level=logging.DEBUG)
@@ -33,39 +34,73 @@ class MyServer(object):
 
         # model = node_topology()
         # model.node.add("10.1.7.64")
-        #
-        #
+        
+        
         # data = util.elm("nc:data")
         # sysc = util.subelm(data, "node-topology:node")
         # sysc.append(util.leaf_elm("node-topology:node-id", '10.1.7.64'))
         # nose = util.subelm(sysc, "node-topology:port")
         # nose.append(util.leaf_elm("node-topology:port-id", '01'))
-        #
-        # data2 = pybindIETFXMLEncoder.serialise(model)
-        # print(data2)
-        # print(data)
+        # #
+        # # data2 = pybindIETFXMLEncoder.serialise(model)
+        # # print(data2)
+        # print(ElementTree.tostring(data))
 
-        nt = node_topology()
-        nt.node.add("10.1.7.64")
-        nt.node.add("10.1.7.65")
+        # nt = node_topology()
+        # nt.node.add("10.1.7.64")
+        # nt.node.add("10.1.7.65")
 
-        for i, n in nt.node.iteritems():
-            n.port.add("1")
-            for j, p in n.port.iteritems():
-                p.available_core.add("01")
+        # for i, n in nt.node.iteritems():
+        #     n.port.add("1")
+        #     for j, p in n.port.iteritems():
+        #         p.available_core.add("01")
 
-        b = pybindIETFXMLEncoder.serialise(nt)
+        # b = pybindIETFXMLEncoder.serialise(nt)
         # print(b)  # xml
 
         # data = ElementTree.Element("nc:data")
         # data.append(ElementTree.Element(b))
         # print(ElementTree.tostring(data))
 
-        data = util.elm("nc:data")
-        data.append(util.leaf_elm("node-topology:node", b))
-        print(ElementTree.fromstring(data))
+        # data = util.elm("nc:data")
+        # data.append(util.leaf_elm("node-topology:node", b))
+        # print(ElementTree.fromstring(data))
 
         # return data2
+
+		# nt = node_topology()
+		# nt.node.add("10.1.7.64")
+		# nt.node.add("10.1.7.65")
+
+		# for i, n in nt.node.iteritems():
+		#     n.port.add("1")
+		#     for j, p in n.port.iteritems():
+		#         p.available_core.add("01")
+
+		# data = pybindIETFXMLEncoder.serialise(nt)
+		# # print(data)  # xml
+
+		# write_file('test.xml', data)
+
+		# # tree = ElementTree.parse('test.xml')
+		# # root = tree.getroot()
+		# # newroot = ElementTree.Element("data")
+		# # newroot.insert(0, root)
+		# # newroot.append(root)
+		# with open('test.xml', 'rb') as f:
+		# 	result = f.read()
+  #   		print('<nc:data>{}</nc:data>'.format(result))
+  #   		t = '<data>{}</data>'.format(result)
+
+
+        print("-"*30)
+        tree = ElementTree.parse('test.xml')
+        root = tree.getroot()
+        print(ElementTree.tostring(root))
+        data = util.elm("nc:data")
+        data.append(util.leaf_elm("node-topology:node", ElementTree.tostring(root)))
+        print("-"*30)
+        print(ElementTree.tostring(data))
 
         return util.filter_results(rpc, data, filter_or_none)
 
