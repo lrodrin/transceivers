f1cfedd967dac87dd32e16c55532c88baf673c8f
lrodrin <lrodrin@gmail.com>
2019-01-11 15:38:36 +0100

    script docu generator

--- gen_docu.py
+++ gen_docu.py
@@ -1,6 +1,18 @@
+import glob
+import os
+import shutil
+
 __author__ = "Laura Rodriguez Navas <laura.rodriguez@cttc.cat>"
 __copyright__ = "Copyright 2018, CTTC"
 
 
+def listdirs(folder):
+   return [d for d in os.listdir(folder) if os.path.isdir(os.path.join(folder, d))]
+
 if __name__ == '__main__':
-    pass
+    basepath = os.getcwd()
+    libpath = "/lib"
+    ld = listdirs(basepath + libpath)
+    print(ld)
+    for d in ld:
+        os.system("pydoc -w " + d + "/*.py")
