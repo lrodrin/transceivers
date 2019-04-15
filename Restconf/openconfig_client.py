59ca610509b6edd7605b8d492544e8d217785e0a
lrodriguez <laura.rodriguez@cttc.cat>
2019-04-15 10:28:02 +0200

    update metrohaul configuration files

--- Restconf/openconfig_client.py
+++ Restconf/openconfig_client.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     ip = "10.1.7.64"
     headers = {"Content-Type": "application/json"}
     params_lca = {'client': 'c1', 'och': 'och1'}
-    params_occ = {'och': 'och1', 'freq': 193.4e6, 'power': 0.4, 'mode': 'HD-FEC'}
+    params_occ = {'och': 'och1', 'freq': 193.4e6, 'power': 0, 'mode': 'HD-FEC'}
 
     # local channel assignment
     request = requests.post('http://%s:5000/api/vi/openconfig/local_channel_assignment' % ip, headers=headers,
